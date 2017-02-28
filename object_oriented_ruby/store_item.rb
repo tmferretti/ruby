3fa6c9e45bdd3f7791d0c78ae3ac0c421702052e
Michael <michael.ferretti@outlook.com>
2017-02-27 20:15:28 -0500

    Add class file

--- object_oriented_ruby/store_item.rb
+++ object_oriented_ruby/store_item.rb
@@ -1,22 +1,16 @@
-obj1 = {
-  desc: "Model Airplane",
-  color: "blue",
-  price: 19.99,
-  stock_status: true
-}
+class Employee
+  def initialize(input_first, input_last, input_salary, input_active)
+    @input_first = input_first
+    @input_last = input_last
+    @input_salary = input_salary
+    @input_active = input_active
+  end
+  def print_info
+    p "#{@input_first} #{@input_last} makes #{@input_salary} per year."
+  end
+end
 
-obj2 = {
-  :desc => "Screw Driver",
-  :color => "Red",
-  :price => 5.99,
-  :stock_status => false
-}
-
-obj3 = {
-  :desc => "Water Pipe",
-  :color => "clear",
-  :price => 149.99,
-  :stock_status => true
-}
-
-p "The #{obj3[:desc]} is #{obj3[:price]}."
+employee1 = Employee.new("Manila", "Campos", 80000, true)
+employee2 = Employee.new("Danillo", "Carter", 70000, false)
+employee1.print_info
+employee2.print_info
